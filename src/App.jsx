6f5c409e0a03ce68e6726f11fc7190d9cd4ffa5c
Valerie <valeriejeptoo4@gmail.com>
2023-08-03 00:49:13 +0300

    added sotr

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,65 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BotCollection from './Components/BotCollection';
+import YourBotArmy from './Components/YourBotArmy';
+import BotCard from './Components/BotCard';
+import BotSpecs from './Components/BotSpecs';
+import SortBar from './Components/SortBar';
 
-import './App.css'
-import BotCollection from './Components/BotCollection'
-import YourBotArmy from './Components/YourBotArmy'
-import BotCard from './Components/BotCard'
-import BotSpecs from './Components/BotSpecs'
-import SortBar from './Components/SortBar'
-
-// implemented useState  
 function App() {
-  const [originalBots, setOriginalBots] = useState([])
-  const [botArmy, setBotArmy] = useState([])
+  const [originalBots, setOriginalBots] = useState([]);
+  const [botArmy, setBotArmy] = useState([]);
 
   useEffect(() => {
-    fetch('https://valerie.onrender.com/bots').then((res) => res.json()).then((data) => {
-        setOriginalBots(data)
-    })
-}, [])
+    fetch('https://valerie.onrender.com/bots')
+      .then((res) => res.json())
+      .then((data) => {
+        setOriginalBots(data);
+      });
+  }, []);
 
   const displayBots = (bots, handleBotClick) => {
-    const newBotsArray = bots.map((bot) => {
-      return (
-          <BotCard key={bot.id} bot={bot} originalBots={originalBots} botArmy={botArmy} setBotArmy={setBotArmy} handleBotClick={handleBotClick} setOriginalBots={setOriginalBots}/>
-      )
-    })
+    return bots.map((bot) => (
+      <BotCard
+        key={bot.id}
+        bot={bot}
+        originalBots={originalBots}
+        botArmy={botArmy}
+        setBotArmy={setBotArmy}
+        handleBotClick={handleBotClick}
+        setOriginalBots={setOriginalBots}
+      />
+    ));
+  };
 
-    return newBotsArray
-  }
+  const sortBotsByHealth = () => {
+    const sortedBots = [...originalBots].sort((a, b) => b.health - a.health);
+    setOriginalBots(sortedBots);
+  };
+
+  const sortBotsByDamage = () => {
+    const sortedBots = [...originalBots].sort((a, b) => b.damage - a.damage);
+    setOriginalBots(sortedBots);
+  };
+
+  const sortBotsByArmor = () => {
+    const sortedBots = [...originalBots].sort((a, b) => b.armor - a.armor);
+    setOriginalBots(sortedBots);
+  };
 
   return (
-    
-    
-    
     <BrowserRouter>
-      <YourBotArmy botArmy={botArmy} setBotArmy={setBotArmy} displayBots={displayBots}/>
-     {/* Render the sort buttons */}
-      <SortBar
-    onSortByHealth={sortBotsByHealth}
-    onSortByDamage={sortBotsByDamage}
-    onSortByArmor={sortBotsByArmor}
-  />
-
+      <YourBotArmy botArmy={botArmy} setBotArmy={setBotArmy} displayBots={displayBots} />
+      <SortBar onSortByHealth={sortBotsByHealth} onSortByDamage={sortBotsByDamage} onSortByArmor={sortBotsByArmor} />
       <Routes>
-        <Route path="/" element={<BotCollection originalBots={originalBots} botArmy={botArmy} setBotArmy={setBotArmy} displayBots={displayBots} />} />
-        {/* the :currentBotId show that anything after / is a value stored in currentBotId variable */}
+        <Route
+          path="/"
+          element={<BotCollection originalBots={originalBots} botArmy={botArmy} setBotArmy={setBotArmy} displayBots={displayBots} />}
+        />
         <Route path="/:currentBotId" element={<BotSpecs originalBots={originalBots} botArmy={botArmy} setBotArmy={setBotArmy} />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/SortBar.jsx
+++ src/Components/SortBar.jsx
@@ -1,16 +1,22 @@
-// Allows damage, or  the user to sort by health,damage or armor 
-import React from "react";
+import React from 'react';
 
-    const SortBar = ({onSortByHealth, onSortByDamage, onSortByArmor})=> {
-        return(
-            <div className="sortbar">
-                {/* created buttons to sort the bots */}
-                <button onClick={onSortByHealth}>Sort by Health</button>
-                <button onClick={onSortByDamage}>Sort by Damage</button>
-                <button onCLICK={onSortByArmor}>Sort by Armor</button>
-            </div>
-        );
-    };
-    export default SortBar;
+// SortBar component that allows the user to sort bots by health, damage, or armor
+// The component receives `onSortByHealth`, `onSortByDamage`, and `onSortByArmor` as props
+const SortBar = ({ onSortByHealth, onSortByDamage, onSortByArmor }) => {
+  return (
+    // Render a container with buttons for sorting bots
+    <div className="sort-bar">
+      {/* Button to sort bots by health */}
+      <button onClick={onSortByHealth}>Sort by Health</button>
 
+      {/* Button to sort bots by damage */}
+      <button onClick={onSortByDamage}>Sort by Damage</button>
 
+      {/* Button to sort bots by armor */}
+      <button onClick={onSortByArmor}>Sort by Armor</button>
+    </div>
+  );
+};
+
+// Export the SortBar component for use in other files
+export default SortBar;
